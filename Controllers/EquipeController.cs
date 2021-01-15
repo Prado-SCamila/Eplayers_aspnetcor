55067d7fc01beedc144cc871a2d3988465f7506c
Prado-SCamila <prado.camila.910423@gmail.com>
2021-01-15 13:02:19 -0300

    Parte Controllers incluída

---
+++ Controllers/EquipeController.cs
@@ -0,0 +1,37 @@
+using Eplayers_aspnetcor.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+namespace Eplayers_aspnetcor.Controllers
+{
+
+    [Route("Equipe")]
+    public class EquipeController: Controller
+    {
+        Equipe equipeModel = new Equipe();
+
+
+
+        public IActionResult Index(){
+
+            //Listamos todas as equipes e enviamos para a view, através da viewbag
+            ViewBag.equipes = equipeModel.ReadAll();
+            return View();
+        }
+
+        public IActionResult Cadastrar(IFormCollection form){
+
+            Equipe novaEquipe = new Equipe();
+            novaEquipe.IdEquipe = int.Parse(form["IdEquipe"]);
+            novaEquipe.Nome = form["Nome"];
+            novaEquipe.Imagem = form["Imagem"];
+
+            equipeModel.Create(novaEquipe);
+            ViewBag.equipes = equipeModel.ReadAll();
+
+            //Redirecionar para a mesma página onde estamos. ~ indica root, raiz.
+            return LocalRedirect("~/Equipe");
+        }
+
+
+    }
+}
